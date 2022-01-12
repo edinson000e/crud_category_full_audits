8c0edcd2c597ac5c0bc98531d63568b7befbf0e3
evillazana27 <edinson.villazana@phinxlab.com>
2022-01-12 00:31:41 -0400

    create data array structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState } from "react";
+import "./App.css";
+
+const initialCategories = [
+  {
+    id: 1,
+    name: "test 1",
+    order: 1,
+    category_parent: 0,
+  },
+  {
+    id: 2,
+    name: "test 2",
+    order: 2,
+    category_parent: 0,
+  },
+  {
+    id: 3,
+    name: "test 3",
+    order: 3,
+    category_parent: 0,
+  },
+  {
+    id: 4,
+    name: "test 2.1",
+    order: 1,
+    category_parent: 2,
+  },
+  {
+    id: 5,
+    name: "test 2.1.1",
+    order: 1,
+    category_parent: 4,
+  },
+  {
+    id: 6,
+    name: "test 2.2",
+    order: 1,
+    category_parent: 2,
+  },
+  {
+    id: 7,
+    name: "test 1.1",
+    order: 1,
+    category_parent: 1,
+  },
+  {
+    id: 8,
+    name: "test 1.2",
+    order: 2,
+    category_parent: 1,
+  },
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [categories, setCategories] = useState(initialCategories);
+
+  console.log("setCategories",setCategories)
+  const categoriesWithParent = useMemo(() => {
+
+    return categories.reduce((allCategoriesWithParent:any, category:any)=> {
+
+      const copyAllCategoriesWithParents = [...allCategoriesWithParent]
+
+
+      return copyAllCategoriesWithParents
+    },[])
+
+  }, [categories]);
+
+  console.log("cateogir",categoriesWithParent)
+
+  return <div className="App"></div>;
 }
 
 export default App;
